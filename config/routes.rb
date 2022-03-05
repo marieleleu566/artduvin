201345c978ef39646cdb9a97248efc9d3e971f58
Marie Leleu <marieleleu@icloud.com>
2022-03-05 16:04:13 +0100

    started art du vin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'wineclub', to: 'pages#wineclub'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
