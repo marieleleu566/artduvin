bb17530e214a8e41505b49d2bbc1cff8e318f637
Marie Leleu <marieleleu@icloud.com>
2022-04-06 20:17:25 +0200

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,13 +26,12 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  config.assets.precompile += %w( '.svg' )
 
   # Must include to get inline SVGs to work in deploy
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
